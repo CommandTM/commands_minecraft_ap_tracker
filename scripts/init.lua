8a17f9aa21e42aef37c6c67a4a1a2a1d21efb81a
CommandTM <commandnoobmc23@gmail.com>
2023-06-05 17:59:08 -0400

    Added Advancement Item/Advancements Required

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,8 +1,10 @@
 Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/options.json")
 
 Tracker:AddMaps("maps/maps.json")
 
 Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/options.json")
 
 ScriptHost:LoadScript("scripts/loadlocations.lua")
 
