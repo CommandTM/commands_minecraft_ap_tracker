e03d4bcae0b3f045bf4a75b4924a6cea26a4304e
CommandTM <commandnoobmc23@gmail.com>
2023-07-05 16:28:56 -0400

    Working Boss Goal Logic
    Sadly, You Have To Refresh The Goal Progression Item

--- scripts/goal_logic.lua
+++ scripts/goal_logic.lua
@@ -11,11 +11,32 @@ function EggShards()
 end
 
 function BossAccess()
-	if Tracker:ProviderCountForCode("goal")==1 then
-		if Tracker:FindObjectForCode("@The End?/Advancement Made").AvailableChectCount==1 then
-			return 0
-		else
-			return 1
+	local goal = Tracker:FindObjectForCode("goal")
+	local endAdvancement = Tracker:FindObjectForCode("@The End?/Advancement Made")
+	local netherAdvancement = Tracker:FindObjectForCode("@Withering Heights/Advancement Made")
+	local bossesNeed = 1
+	local bossesAccess = 0
+	if goal.CurrentStage==0 then
+		bossesNeed = 0
+	end
+	if goal.CurrentStage==1 then
+		if endAdvancement.AvailableChestCount==0 then
+			bossesAccess = bossesAccess + 1
 		end
 	end
+	if goal.CurrentStage==2 then
+		if netherAdvancement.AvailableChestCount==0 then
+			bossesAccess = bossesAccess + 1
+		end
+	end
+	if goal.CurrentStage==3 then
+		bossesNeed = 2
+		if endAdvancement.AvailableChestCount==0 then
+			bossesAccess = bossesAccess + 1
+		end
+		if netherAdvancement.AvailableChestCount==0 then
+			bossesAccess = bossesAccess + 1
+		end
+	end
+	return (bossesAccess >= bossesNeed)
 end
