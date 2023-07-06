7976837f3ffdb2ea468e05e17e475ac19237f6c2
CommandTM <commandnoobmc23@gmail.com>
2023-07-05 22:06:30 -0400

    Started Location Mapping

---
+++ scripts/mapping/location_mapping.lua
@@ -0,0 +1,23 @@
+LOCATION_MAPPING = {
+    [42000] = {"@Who Is Cutting Onions/Advancement Made"},
+    [42001] = {"@Oh Shiny/Advancement Made"},
+    [42002] = {"@Suit Up/Advancement Made"},
+    [42003] = {"@Very Very Frightening/Advancement Made"},
+    [42004] = {"@Hot Stuff/Advancement Made"},
+    [42005] = {"@Free the End/Advancement Made"},
+    [42006] = {"@A Furious Cocktail/Challenge Complete"},
+    [42007] = {"@Best Friends Forever/Advancement Made"},
+    [42008] = {"@Bring Home the Beacon/Advancement Made"},
+    [42009] = {"@Not Today, Thank You/Advancement Made"},
+    [42010] = {"@Isn't It Iron Pick/Advancement Made"},
+    [42011] = {"@Local Brewery/Advancement Made"},
+    [42012] = {"@The Next Generation/Goal Reached"},
+    [42013] = {"@Fishy Business/Advancement Made"},
+    [42014] = {"@Hot Tourist Destinations/Challenge Complete"},
+    [42015] = {"@This Boat Has Legs/Advancement Made"},
+    [42016] = {"@Sniper Duel/Challenge Complete"},
+    [42017] = {"@Nether/Advancement Made"},
+    [42018] = {"@Great View From Up Here/Challenge Complete"},
+    [42019] = {"@How Did We Get Here?/Challenge Complete"}
+    -- 20/114, Doing 20 Daily, This Part Sucks
+}
